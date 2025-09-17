5ac1d43f91858219837c384ef62ad5096ecc4439
SanmiaoDiary <wdsj163lht@163.com>
2025-09-17 13:26:05 +0800

    修复了若干bug，添加了一些新功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #2025-7-13
 import sys
 import pygame #导入pygame库
+import math
 
 pygame.init() #初始化pygame
 
@@ -21,7 +22,7 @@ try:
     modapi = water.Mod.loading_mods(screen)#示例化modapi Lading_mods类下的loading_mods方法，并把screen赋值给loading_mods
     print("成功加载mod")
 except Exception as error_information:
-    print(":( 导入modAPI失败")
+    print("【警告】:( 导入modAPI失败")
     print(f"错误原因：{error_information}")
 
 runing = True
@@ -37,24 +38,35 @@ class Mouse(pygame.sprite.Sprite):#创建鼠标类
         self.image_selectable = pygame.image.load(r"image\mouse_selectable.png").convert_alpha()#创建鼠标图片
         self.rect = self.image.get_rect()#获取鼠标的rect对象
         
+        
     def update(self):#更新鼠标
-        if character.character_rect.collidepoint(pygame.mouse.get_pos()):#鼠标在角色上
+        mouse_pos = pygame.mouse.get_pos()#获取鼠标位置
+        if character.character_rect.collidepoint(mouse_pos):#鼠标在角色上
            self.rect.center = (pygame.mouse.get_pos())
-           screen.blit(self.image_selectable,pygame.mouse.get_pos())
+           screen.blit(self.image_selectable,mouse_pos)
         else:
-            screen.blit(self.image,pygame.mouse.get_pos())#绘制鼠标
+            screen.blit(self.image,mouse_pos)#绘制鼠标
     
 
 class Character():#创建角色类
-    all_characters = []
-    selected = False
+    game_all_characters = {0000:"cat",1001:"Sunaookami_Shiroko"}
+    #游戏允许拥有的所有角色，在一个地图里面同样的角色只能出现一个（同个角色不同皮肤也一样）
+    #前面是数字是角色的代号，方便识别角色
+    characters_speed = {0000:5,1001:10}#所有允许存在的角色的移动速度
+    all_characters = []#所有在地图上出现的角色
+    
     def __init__(self,x,y):#初始化
-        # character.all_characters.add(self) = []#把角色名字加入到所有角色列表
+        self.id = self#实例的名字就是角色的id
+        for self in Character.game_all_characters.keys(): #遍历游戏确保设定的角色id是允许拥有角色的id
+            if self.id == self.game_all_characters[self]: #如果角色的id等于游戏允许拥有角色的id
+                print("【错误】角色创建失败：地图上已存在同名角色") #提示角色已经在地图上存在
+            else:        
+                self.all_characters.append(self)#把角色加入到所有角色列表
         self.character = pygame.image.load(r"image\cat.png").convert_alpha()#创建角色图片
         self.selection_marker = pygame.image.load(r".\image\character_selection_marker.png").convert_alpha()#创建一个角色选中标记
         self.character_rect = self.character.get_rect()#获取角色的矩形坐标
         self.character_rect.x,self.character_rect.y = x,y#初始化创建角色的位置
-        self.all_characters.append(self)
+        self.selected = False#设置角色是否被选中
         
     def update(self,screen):#更新角色和选中标记 screen:绘制在哪个窗口上
         if self.selected == True:
@@ -63,11 +75,15 @@ class Character():#创建角色类
 
     def select(self):#角色被选中
         self.clicked = True#设置角色被选中
-        print("角色被选中")
+        print("【提示】角色被选中")
 
     def deselect(self):#角色被取消选中
         self.clicked = False#设置角色被取消选中
-        print("角色被取消选中")
+        print("【提示】角色被取消选中")
+
+pygame.mouse.set_visible(False)#隐藏系统鼠标
+game_mouse = Mouse()#实例化鼠标类
+cat = Character(540, 300)#实例化角色类_小猫
 
 while runing == True: #游戏循环
     for event in pygame.event.get():#获取事件
@@ -84,30 +100,26 @@ while runing == True: #游戏循环
                 else:
                     screen = pygame.display.set_mode((1080,600),pygame.RESIZABLE)
         
-    screen.fill((240, 240, 240))
+        # 是否选中角色逻辑判断
+        for character in Character.all_characters:#遍历所有角色
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #如果鼠标左键被按下
+                if character.character_rect.collidepoint(pygame.mouse.get_pos()): #如果鼠标在角色上
+                    
+                    if character.selected == True: #如果角色被选中那就取消选中
+                        character.deselect()
+                    else:#如果角色没有被选中那就选中角色
+                        character.select()
+                else: #如果鼠标不在角色上
+                    dest_x,dest_y = (pygame.mouse.get_pos())#记录下鼠标位置方便移动
+            
+    screen.fill((240, 240, 240))  
 
-    game_mouse = Mouse()#实例化鼠标类
-    cat = Character(540, 300)#实例化角色类_小猫
-    
-
-    left,middle, right = pygame.mouse.get_pressed()#获取鼠标按键状态
-    # 是否选中角色逻辑判断
-    for character in Character.all_characters:#遍历所有角色
-        if left == True: #如果鼠标左键被按下
-            if character.character_rect.collidepoint(pygame.mouse.get_pos()): #如果鼠标在角色上
-                
-                if character.selected == True: #如果角色被选中那就取消选中
-                    character.deselect()
-                else:#如果角色没有被选中那就选中角色
-                    character.select()
-            else: #如果鼠标不在角色上
-                mouse_x,mouse_y = (pygame.mouse.get_pos())#记录下鼠标位置方便移动
-        
-        cat.update(screen)
-    game_mouse.update()    
+    cat.update(screen)
+     
     show_fps = font.render("FPS: " + str(int(clock.get_fps())), True,(0, 0, 0),(255,225,225))
     textRect =show_fps.get_rect()#获取文字的矩形坐标
     textRect.center = (40, 10)#设置文字位置和坐标
     screen.blit(show_fps, textRect)#绘制文字
+    game_mouse.update()#在显示fps之后更新鼠标 防止fps挡住鼠标  
     pygame.display.update()#更新屏幕
     clock.tick(fps)
